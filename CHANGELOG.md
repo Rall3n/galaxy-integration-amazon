eb9c6dcdd5483c219017d4241374000dde7f3d34
Rallen <rallen@gruppe-w.de>
2020-07-09 00:05:04 +0200

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,8 @@
 # Changelog
 
+## 0.1.1
+- Add OSCompatability check to allow installation of games
+
 ## 0.1.0
 - Initial Version
 
--- src/version.py
+++ src/version.py
@@ -1,6 +1,9 @@
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __changelog__ = {
+    "0.1.1": '''
+        - Add OSCompatability check to allow installation of games
+    ''',
     "0.1.0": '''
         - Initial Version
     '''
