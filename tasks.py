9f3803b184a9678e7e985d24e818288a6153a7e1
Rallen <rallen@gruppe-w.de>
2022-05-22 17:26:09 +0200

    feat(develop): Updated build tasks

--- tasks.py
+++ tasks.py
@@ -6,9 +6,7 @@ import tempfile
 import shutil
 from termcolor import colored
 from pathlib import Path
-from distutils.dir_util import copy_tree, remove_tree
-from glob import glob
-from fog import buildtools
+from fog.buildtools import update_changelog_file
 from galaxy.tools import zip_folder_to_file
 
 from invoke import task, call
@@ -19,14 +17,18 @@ REPO_PATH = Path(__file__).resolve().parent
 DIST_PATH = Path(REPO_PATH, "dist").resolve()
 SRC_PATH = Path(REPO_PATH, "src").resolve()
 FOG_RELEASE_PATH = Path(REPO_PATH, "fog_release").resolve()
-GALAXY_PATH = 'C:\\Program Files (x86)\\GOG Galaxy\\GalaxyClient.exe'
+GALAXY_PATH = f"{os.environ['programfiles(x86)']}\\GOG Galaxy\\GalaxyClient.exe"
 PLUGIN_PATH = f"{os.environ['localappdata']}\\GOG.com\\Galaxy\\plugins\\installed\\amazon_dev"
 
 PLUGIN_GUID = str(uuid.uuid3(uuid.NAMESPACE_DNS, 'Rall3n/galaxy-integration-amazon'))
 
+PIP_PLATFORM = 'win32'
+
 
 @task(optional=['output'])
 def build_manifest(c, output=str(DIST_PATH)):
+    out_path = Path(output)
+
     print(f'[{colored("TASK", "yellow")}] Generating manifest ...')
 
     manifest = {
@@ -41,30 +43,51 @@ def build_manifest(c, output=str(DIST_PATH)):
         "script": "plugin.py"
     }
 
-    with open(os.path.join(output, "manifest.json"), "w") as file_:
+    with out_path.joinpath('manifest.json').open('w') as file_:
         json.dump(manifest, file_, indent=4)
 
 
 @task(optional=['output'])
 def build(c, output=str(DIST_PATH)):
+    out_path = Path(output)
+
     print(f'[{colored("TASK", "yellow")}] Building plugin ...')
     try:
-        with tempfile.NamedTemporaryFile(mode="r+", delete=False) as tmp:
-            tmp.write(c.run('pipenv lock -r', hide=True).stdout)
-            tmp.seek(0)
+        shutil.rmtree(output, ignore_errors=True)
 
-            buildtools.build(src='src', output=output, requirements=tmp.name)
-            
-            # buildtools.build only copies *.py files
-            if Path(output, 'splash').resolve().exists():
-                shutil.rmtree(Path(output, 'splash').resolve())
+        with tempfile.NamedTemporaryFile(mode="r+", delete=False) as pipenvLockTmp:
+            pipenvLockTmp.write(c.run('pipenv lock -r', hide=True).stdout)
+            pipenvLockTmp.seek(0)
 
-            shutil.copytree('src/splash', Path(output, 'splash').resolve())
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False) as pipReqTmp:
+            c.run(f'pipenv run pip-compile {Path(pipenvLockTmp.name).as_posix()} --output-file=-', out_stream=pipReqTmp, hide=True)
+            pipReqTmp.seek(0)
 
-            build_manifest(c, output)
+        args = [
+            'pip', 'install',
+            '-r', pipReqTmp.name,
+            '--platform', PIP_PLATFORM,
+            '--target', out_path.as_posix(),
+            '--python-version', '37',
+            '--no-compile',
+            '--no-deps'
+        ]
+
+        c.run(' '.join(args), echo=True, hide=True)
+
+        for dir in out_path.glob('*.dist-info'):
+            shutil.rmtree(dir)
+        for test in [*out_path.rglob('test_*.py'), *out_path.rglob('*_test.py')]:
+            test.unlink()
+
+        shutil.copytree(SRC_PATH, Path(output).resolve(), dirs_exist_ok=True)
+
+        build_manifest(c, output)
     finally:
-        if tmp:
-            os.unlink(tmp.name)
+        if pipenvLockTmp:
+            os.unlink(pipenvLockTmp.name)
+        if pipReqTmp:
+            os.unlink(pipReqTmp.name)
 
 
 @task
@@ -83,19 +106,21 @@ def deploy(c, galaxy_path=GALAXY_PATH):
     build(c, output=PLUGIN_PATH)
 
     print(f'[{colored("TASK", "yellow")}] Reopening Galaxy from {galaxy_path} ...')
-    c.run(galaxy_path, asynchronous=True, hide=True)
+    with c.cd('C:\\Windows\\System32'):
+        c.run(f'"{galaxy_path}"', asynchronous=True, hide=True)
 
 
 @task(optional=['out_dir'])
 def update_changelog(c, out_dir=str(REPO_PATH)):
-    buildtools.update_changelog_file(repo_path=str(REPO_PATH), out_dir=out_dir)
+    update_changelog_file(repo_path=str(REPO_PATH), out_dir=out_dir)
 
 
 @task(pre=[call(build)], optional=['archive_name'])
 def dist(c, archive_name=f'amazon_{PLUGIN_GUID}.zip'):
+    archive_path = Path(archive_name).resolve()
+
     print(f'[{colored("TASK", "yellow")}] Creating zipped file for distribution ...')
-    if Path(archive_name).resolve().exists():
-        Path(archive_name).resolve().unlink()
+    archive_path.unlink(missing_ok=True)
 
     update_changelog(c)
 
@@ -104,18 +129,22 @@ def dist(c, archive_name=f'amazon_{PLUGIN_GUID}.zip'):
 
 @task(optional=['output'])
 def update_fog_release(c, output=str(FOG_RELEASE_PATH)):
+    out_path = Path(output)
+
     print(f'[{colored("TASK", "yellow")}] Pushing to fog_release branch ...')
 
-    copy_tree(str(SRC_PATH), output)
-    build_manifest(c, output=output)
-    create_requirements_file(c, output=output)
+    shutil.copytree(SRC_PATH, out_path, dirs_exist_ok=True)
+    build_manifest(c, output=out_path)
+    create_requirements_file(c, output=out_path)
 
     print(f'[{colored("TASK", "yellow")}] Copying `current_version.json` ...')
-    shutil.copy2(str(Path(REPO_PATH, 'current_version.json')), output)
+    shutil.copy2(str(Path(REPO_PATH, 'current_version.json')), out_path)
 
 
 @task
 def create_requirements_file(c, output=str(REPO_PATH)):
+    out_path = Path(output)
+
     print(f'[{colored("TASK", "yellow")}] Generating `requirements.txt` from Pipfile.lock ...')
     packages = []
     root = None
@@ -130,5 +159,5 @@ def create_requirements_file(c, output=str(REPO_PATH)):
         version = pkg["version"]
         packages.append(f'{name}{version}')
 
-    with open(os.path.join(output, 'requirements.txt'), 'w') as _file:
+    with out_path.joinpath('requirements.txt').open('w') as _file:
         _file.write('\n'.join(packages))
