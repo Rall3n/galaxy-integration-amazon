a435e3e9015ee51151f09abf062dccf716a7cb25
Rallen <rallen@gruppe-w.de>
2022-05-26 19:00:24 +0200

    v0.4.3

--- db_client.py
+++ db_client.py
@@ -1,10 +1,14 @@
+from logging import Logger, getLogger
 import sqlite3
 
 from contextlib import closing
 
 class DBClient:
+    logger: Logger
+
     def __init__(self, path):
         self.path = path
+        self.logger = getLogger('amazonPlugin')
 
     def select(self, table, rows=['*'], where='1'):
         with closing(sqlite3.connect(self.path)) as conn:
@@ -14,4 +18,5 @@ class DBClient:
                     cursor.execute(f'SELECT {", ".join(rows)} FROM {table} WHERE {where};')
                     return cursor.fetchall()
                 except sqlite3.DatabaseError as e:
+                    self.logger.exception(f'DB exception encountered while trying to read rows "{", ".join(rows)}" from table "{table}": {e}')
                     return []
